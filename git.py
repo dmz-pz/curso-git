c2bc911bf73d384849c5b472727e4823bf85d9b4
dmz <dm799166@gmail.com>
2025-07-18 09:53:24 -0400

    Este es mi segundo commit agregando el fichero git

---
+++ git.py
@@ -0,0 +1,8 @@
+# Para inicializar el contexto de git en un directorio 
+# Ejecutamo el comando: git init
+
+# Para empezar a guardar o manejar el control de nuestros primero hay que agregarlos
+# Mediante el comando git add <nombre del fichero> 
+
+# Para empezar a tomar la primera foto o lo que vendria siendo el primer cambio para el control de versiones 
+# Lo que hacemos es lo que vendria siendo "commit" mediante el comando 'git commit'
